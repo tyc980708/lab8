64bec1ffe13aa09a2685f8589d95a9fbeda69b2e
Tianyi Cao <tic099@ucsd.edu>
2018-02-28 21:28:54 -0800

    lab8!

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+  var ucsd_ltlng = {lat:32.8788562, lng:-117.2445998};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_ltlng,
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+    position: ucsd_ltlng,
+    map: map,
+    title: "UCSD"
+  });
+
 }
